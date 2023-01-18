61020358f6792f28e7e69d9e4b67c90ea96b35c8
dennisgbrown <dennis.g.brown@gmail.com>
2023-01-17 21:05:56 -0600

    send parameters via post

--- Assets/Scripts/DataClients/NexusClient.cs
+++ Assets/Scripts/DataClients/NexusClient.cs
@@ -44,20 +44,33 @@ public class NexusClient  // : MonoBehaviour  // Later we might want to make thi
 
     public async void PingNexus()
     {
-        FileStats fileStats = await GetFileStats();
+        FileStats fileStats = await GetFileStats(12345);
         Debug.Log("NexusClient DATA: " + fileStats.whatever);
         Debug.Log("NexusClient COUNTER: " + uselessCounter);
     }
 
-    private async Task<FileStats> GetFileStats()
+    private async Task<FileStats> GetFileStats(int userId)
     {
         //Debug.Log("NexusClient GETFILESTATS ENTER");
+
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:5000/filestats");
+        // request.keepAlive = false;
+        request.ContentType = "application/json";
+        request.Method = "POST";
+        StreamWriter writer = new StreamWriter(request.GetRequestStream());
+        string jsonRequest = "{\"userId\":\"" + userId + "\"}";
+        Debug.Log("REQUEST:" + jsonRequest);
+        writer.Write(jsonRequest);
+        writer.Close();
+
         HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
         StreamReader reader = new StreamReader(response.GetResponseStream());
         string jsonResponse = reader.ReadToEnd();
+        reader.Close();
         response.Close();
+
         FileStats fileStats = JsonUtility.FromJson<FileStats>(jsonResponse);
+
         //Debug.Log("NexusClient GETFILESTATS EXIT");
         return fileStats;
     }
