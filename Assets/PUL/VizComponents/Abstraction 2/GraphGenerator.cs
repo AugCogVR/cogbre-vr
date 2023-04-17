7eca1aa483bb5fbded497e8b0987db470ebb6ca5
unknown <jcb0209@auburn.edu>
2023-04-17 17:30:11 -0500

    Added Graph Organization Functionality

--- Assets/PUL/VizComponents/Abstraction 2/GraphGenerator.cs
+++ Assets/PUL/VizComponents/Abstraction 2/GraphGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GraphGenerator : MonoBehaviour
@@ -7,12 +8,13 @@ public class GraphGenerator : MonoBehaviour
     public GameObject nodePrefab;
     public GameObject edgePrefab;
     public List<GameObject> totalNodes = new List<GameObject>();
+    public List<GameObject> totalEdges = new List<GameObject>();
 
     //adds node to graph and stores the node in the list "total nodes"
     public void AddNodeToGraph(string nodeName, string nodeValue)
     {
-        Vector3 objPosition = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0);
-        GameObject node = Object.Instantiate(nodePrefab, objPosition, Quaternion.identity);
+        
+        GameObject node = Object.Instantiate(nodePrefab);
         Node nodeComponent = node.GetComponent<Node>();
         nodeComponent.nodeName = nodeName;
         nodeComponent.nodeValue = nodeValue;
@@ -26,10 +28,114 @@ public class GraphGenerator : MonoBehaviour
         {
             GameObject newEdge = Object.Instantiate(edgePrefab);
             parentNode.GetComponent<Node>().AddChild(node, newEdge);
+            totalEdges.Add(newEdge);
             node.GetComponent<Node>().AddParent(parentNode, newEdge);
         }
     }
 
+    //perfoms a level order traversal down the tree of nodes and returns a list of all the node layers' lengths in a convenient list.
+    public void SetLayerList(ref List<List<GameObject>> layerNodes, GameObject startingNode)
+    {
+        List<List<GameObject>> ans = new List<List<GameObject>>();
+
+        if (startingNode == null)
+            Debug.Log("N-Ary tree does not any nodes");
+
+        // Create one queue main_queue
+        Queue<GameObject> main_queue = new Queue<GameObject>();
+        HashSet<GameObject> visited = new HashSet<GameObject>();
+
+        // Push the root value in the main_queue
+        main_queue.Enqueue(startingNode);
+        visited.Add(startingNode);
+
+        // Traverse the N-ary Tree by level
+        while (main_queue.Any())
+        {
+            // Create a temp vector to store the all the
+            // node values present at a particular level
+            List<GameObject> temp = new List<GameObject>();
+            int size = main_queue.Count;
+            // Iterate through the current level
+            for (int i = 0; i < size; i++)
+            {
+                GameObject node = main_queue.Dequeue();
+                temp.Add(node);
+
+                foreach (KeyValuePair<GameObject, GameObject> childNode in node.GetComponent<Node>().childNodes)
+                {
+                    if (!visited.Contains(childNode.Key))
+                    {
+                        main_queue.Enqueue(childNode.Key);
+                        visited.Add(childNode.Key);
+                    }
+                }
+            }
+
+            ans.Add(temp);
+        }
+
+        foreach(List<GameObject> list in ans)
+        {
+            Debug.Log(list.ToString());
+            layerNodes.Add(list);
+        }
+
+        
+    }
+
+    public void ArrangeGraph(GameObject startNode, GameObject exitNode)
+    {
+        List<List<GameObject>> layerNodes = new List<List<GameObject>>();
+        SetLayerList(ref layerNodes, startNode);
+        float yTransform = layerNodes.Count();
+
+        startNode.transform.position = new Vector3(0, yTransform, 0);
+        exitNode.transform.position = new Vector3(0, 0, 0);
+
+        
+        Debug.Log(layerNodes.Count());
+
+        foreach(List<GameObject> nodes in layerNodes)
+        {
+           bool noStartOrExitPresent = true;
+            foreach(GameObject node in nodes)
+            {
+                if(node.GetComponent<Node>().nodeName == startNode.GetComponent<Node>().nodeName || node.GetComponent<Node>().nodeName == exitNode.GetComponent<Node>().nodeName)
+                {
+                    Debug.Log("Start or Exit Present!");
+                    noStartOrExitPresent = false;
+                }
+
+            }
+            Debug.Log("Continuing Onward!");
+            if (noStartOrExitPresent)
+            {
+                Debug.Log("Line 107 If Statement Works!");
+                float xTransform = (-1)*(nodes.Count()/2);
+                foreach(GameObject node in nodes)
+                {
+                    node.transform.position = new Vector3(xTransform, yTransform, 0);
+                    xTransform += 1;
+                }
+            }
+            yTransform = yTransform / 2;
+
+        }
+
+    }
+
+    public void DestroyGraph()
+    {
+        foreach(GameObject node in totalNodes)
+        {
+            Object.Destroy(node);
+        }
+        foreach (GameObject edge in totalEdges)
+        {
+            Object.Destroy(edge);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/PUL/VizComponents/Abstraction 2/TempGraphCreator.cs
+++ Assets/PUL/VizComponents/Abstraction 2/TempGraphCreator.cs
@@ -12,17 +12,29 @@ public class TempGraphCreator : MonoBehaviour
         graphGenerator.AddNodeToGraph("0", "Stuff!");
         graphGenerator.AddNodeToGraph("1", "Stuff!");
         graphGenerator.AddNodeToGraph("2", "Stuff!");
+        graphGenerator.AddNodeToGraph("3", "Stuff!");
 
         GameObject g1 = graphGenerator.totalNodes[0];
         GameObject g2 = graphGenerator.totalNodes[1]; 
-        GameObject g3 = graphGenerator.totalNodes[2]; 
+        GameObject g3 = graphGenerator.totalNodes[2];
+        GameObject g4 = graphGenerator.totalNodes[3];
 
-        
+
 
         List <GameObject> g1Children = new List<GameObject>();
+        List<GameObject> g4Children = new List<GameObject>();
+        List<GameObject> g2and3Children = new List<GameObject>();
         g1Children.Add(g2);
         g1Children.Add(g3);
-       graphGenerator.AssignChildrenToNode(g1, g1Children);
+        g2and3Children.Add(g4);
+        g4Children.Add(g1);
+        graphGenerator.AssignChildrenToNode(g1, g1Children);
+        graphGenerator.AssignChildrenToNode(g2, g2and3Children);
+        graphGenerator.AssignChildrenToNode(g2, g4Children);
+        graphGenerator.AssignChildrenToNode(g3, g2and3Children);
+        graphGenerator.AssignChildrenToNode(g4, g4Children);
+        graphGenerator.ArrangeGraph(g1, g4);
+        //graphGenerator.DestroyGraph();
 
     }
    
