5291aa6f1fe4fa4e53f120a7be66f841c4e78328
unknown <jcb0209@auburn.edu>
2023-08-29 14:07:51 -0500

    trying this again

--- Assets/PUL/DataClients/NexusClient.cs
+++ Assets/PUL/DataClients/NexusClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.IO;
 using System.Threading.Tasks;
@@ -7,52 +8,14 @@ using UnityEngine;
 using Newtonsoft.Json;
 using System.Linq;
 
-namespace PUL
+
+namespace PUL2
 {
-    [System.Serializable]
-    public class OxideBasicBlock
-    {
-        public int first_insn { get; set; }
-        public int last_insn { get; set; }
-        public int num_insns { get; set; }
-        public IList<int> members { get; set; }
-        public IList<int> targets { get; set; }
-        public string hash { get; set; }
-        public IDictionary<string, string> insns {get; set; }
-    }
-
 
     [System.Serializable]
-    public class OxideBasicBlocks
+    public class ActiveOxideData
     {
-        public IDictionary<string, OxideBasicBlock> basic_blocks { get; set; }
-    }
-
-
-    [System.Serializable]
-    public class CompVizBlock
-    {
-        public IList<string> lines { get; set; }
-        public IList<string> targets { get; set; }
-    }
-
-
-    [System.Serializable]
-    public class CompVizStage
-    {
-        public string type { get; set; }
-        public string id { get; set; }
-        public IDictionary<string, string> code { get; set; }
-        public IDictionary<string, CompVizBlock> blocks { get; set; }
-    }
-
-
-    [System.Serializable]
-    public class CompVizStages
-    {
-        public IList<CompVizStage> stages { get; set; }
-        //innermost list: [StageID, BlockID], Second list: List<[StageID, BlockID]> relations between blocks and stages, Third List: Container of All Relations
-        public IList<IList<IList<string>>> blockRelations { get; set; }
+        public IList<string> CIDs { get; set; }
     }
 
 
@@ -62,16 +25,14 @@ namespace PUL
 
         public int pacingCounter;
 
-        public float timeBeforeNodesAnchor = 5f;
-
         private string userId;
 
-       
 
-        public NexusClient(GameManager gameManager)
+
+        public NexusClient (GameManager gameManager)
         {
             //Debug.Log("NexusClient Constructor");
-           
+
             this.gameManager = gameManager;
 
             pacingCounter = 0;
@@ -94,139 +55,19 @@ namespace PUL
             }
         }
 
-      public async void NexusSessionInit()
+        public async void NexusSessionInit()
         {
             string sessionInitResult = await NexusSyncTask(userId, "session_init");
-
-            // OXIDE
-            // Get basic program blocks from Nexus
-            //string oxideBasicBlocksJSON = await NexusSyncTask(userId, "get_canned_oxide_program elf_fib_recursive");
-            //OxideBasicBlocks oxideBasicBlocks = new OxideBasicBlocks();
-            //oxideBasicBlocks.basic_blocks = JsonConvert.DeserializeObject<IDictionary<string, OxideBasicBlock>>(oxideBasicBlocksJSON);
-
-
-            // COMPILER VISUALIZATION
-
-            string compVizStagesJSON = await NexusSyncTask(userId, "get_compviz_stages perfect-func");
-            //Debug.Log("COMP VIZ " + compVizStagesJSON);
-            CompVizStages cvs = JsonConvert.DeserializeObject<CompVizStages>(compVizStagesJSON);
-            this.gameManager.cvs = cvs;
-            setNumberOfGraphs(cvs);
-        }
-
-        public void setNumberOfGraphs(CompVizStages cvs)
-        {
-        this.gameManager.graphTotal = cvs.stages.Count;
-        }
-
-        public void buildGraph2FromOxideBlocks(CompVizStages oxideBlockDict, ref List<GameObject> graphHolders)
-        {
-           
-            int graphHolderIndex = 0;
-          
+            string activeOxideData = await NexusSyncTask(userId, "oxide_collection_names");
             
-            foreach (CompVizStage stage in oxideBlockDict.stages) { 
-                foreach(KeyValuePair<string, CompVizBlock> node in stage.blocks)
-                {
-                    graphHolders[graphHolderIndex].GetComponent<Graph>().nodePrefab = Resources.Load("Prefabs/GraphNode") as GameObject;
-                    graphHolders[graphHolderIndex].GetComponent<Graph>().edgePrefab = Resources.Load("Prefabs/Edge") as GameObject;
-
-                    //derives all code values
-                    List<string> codeValues = new List<string>();
-                    List<string> lineNumbers = new List<string>();
-                   
-                    foreach (string lineNumber in node.Value.lines)
-                    {
-                        //we can assume line numbers to be ordered, since we're using it to obtain the correct value from stage.code
-                        codeValues.Add(stage.code[lineNumber]);
-                        lineNumbers.Add(lineNumber);
-                    }
-
-                    graphHolders[graphHolderIndex].GetComponent<Graph>().AddNodeToGraph(node.Key, codeValues, gameManager);
-                    graphHolders[graphHolderIndex].GetComponent<Graph>().totalNodes[node.Key].transform.parent = graphHolders[graphHolderIndex].transform;
-                    graphHolders[graphHolderIndex].GetComponent<Graph>().totalNodes[node.Key].transform.position = graphHolders[graphHolderIndex].GetComponent<Graph>().totalNodes[node.Key].transform.parent.position;
-                }
             
-                graphHolderIndex += 1;
-            }
-
-            //this is an absolutely disgusting function. this works but lets see if we can come back to this and make it cleaner.
-            //get every graph inside graphHolders
-           
-            
-            foreach (GameObject graphHolder in graphHolders)
-            {
-                //create a list to store whether we have visited this node before.
-                //initialize to contain every node, and ensure that no other node has been traveled to before.
-                Dictionary<GameObject, bool> hasVisited = new Dictionary<GameObject, bool>();
-                foreach(KeyValuePair<string, GameObject> node in graphHolder.GetComponent<Graph>().totalNodes)
-                {
-                    Debug.Log(node.Key);
-                    hasVisited.Add(node.Value, false);
-                }
-                
-                //then, get every information block from the json
-                foreach (CompVizStage stage in oxideBlockDict.stages)
-                {
-                    foreach (KeyValuePair<string, CompVizBlock> node in stage.blocks)
-                    {
-                        //next, compare the information stored in the node game objects with the information blocks
-                        foreach (KeyValuePair<string, GameObject> nodeComparison in graphHolder.GetComponent<Graph>().totalNodes)
-                        {
-                            //Debug.LogWarning(nodeComparison.Key);
-                            //if this node targets another node, add it to the list.
-                            if (node.Value.targets.Contains(nodeComparison.Key))
-                            {
-                              //  Debug.Log("Line 171 If Statement Works!");
-                                GameObject parentNode = graphHolder.GetComponent<Graph>().totalNodes[node.Key];
-                                //Debug.Log("Line 172 If Statement Works!");
-                                GameObject edgePrefab = Object.Instantiate(graphHolder.GetComponent<Graph>().edgePrefab);
-                                parentNode.GetComponent<Node>().AddChild(nodeComparison.Value, edgePrefab);
-                                nodeComparison.Value.GetComponent<Node>().AddParent(parentNode, edgePrefab);
-                                hasVisited[parentNode] = true;
-                            }
-                        }
-                    
-                    }
-                }
-                
-                foreach(KeyValuePair<string, GameObject> node in graphHolder.GetComponent<Graph>().totalNodes)
-                {
-                    List<GameObject> childNodes = new List<GameObject>();
-                    foreach (KeyValuePair<GameObject, GameObject> childNode in node.Value.GetComponent<Node>().childNodes) 
-                    {
-                        //Debug.Log(childNode.Key);
-                        childNodes.Add(childNode.Value);
-                    }
-                    //graphHolder.GetComponent<Graph>ChildrenToNode(node.Value, childNodes);
-
-                }
-
-                GameObject StartNode = graphHolder.transform.GetChild(graphHolder.transform.childCount - 1).gameObject;
-                GameObject ExitNode = graphHolder.transform.GetChild(0).gameObject;
-
-                graphHolder.GetComponent<Graph>().ArrangeGraph(ExitNode, StartNode);
+            ActiveOxideData aod = new ActiveOxideData();
+            aod.CIDs = JsonConvert.DeserializeObject<IList<string>>(activeOxideData);
+            this.gameManager.aod = aod;
 
 
-                gameManager.DisplayTextOnPanelView(gameManager.panelView, graphHolders[0]);
-            }
         }
 
-
-
-        private string getOxideCodeString(OxideBasicBlock oxideBlock)
-        {
-            string returnMe = "";
-
-            foreach(string insn in oxideBlock.insns.Values)
-            {
-                returnMe = returnMe + insn + "\n";
-            }
-
-            // Debug.Log(returnMe);
-            return returnMe;
-        }
-         
         private async void NexusUpdate()
         {
             string whatever = await NexusSyncTask(userId, "get_session_update");
@@ -234,30 +75,39 @@ namespace PUL
 
         private async Task<string> NexusSyncTask(string userId, string command)
         {
-            //Debug.Log("NexusSyncTask ENTER");
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:5000/sync_portal");
+                request.ContentType = "application/json";
+                request.Method = "POST";
+                StreamWriter writer = new StreamWriter(await request.GetRequestStreamAsync());
+                string jsonRequest = "{\"userId\":\"" + userId + "\", \"command\":\"" + command + "\"}";
+                writer.Write(jsonRequest);
+                writer.Close();
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:5000/sync_portal");
-            // request.keepAlive = false;
-            request.ContentType = "application/json";
-            request.Method = "POST";
-            StreamWriter writer = new StreamWriter(await request.GetRequestStreamAsync());
-            string jsonRequest = "{\"userId\":\"" + userId 
-                + "\", \"command\":\"" + command + "\"}";
-            // Debug.Log("NexusSync JSON request:" + jsonRequest);
-            writer.Write(jsonRequest);
-            writer.Close();
+                HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string responseStringJson = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
 
-            HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            string responseStringJson = reader.ReadToEnd();
-            reader.Close();
-            response.Close();
-            // Debug.Log("NexusSyncTask cycles used: " + pacingCounter);
+                // Log the JSON response before deserialization for debugging
+                Debug.Log("NexusSync response JSON: " + responseStringJson);
 
-            string responseString = JsonConvert.DeserializeObject<string>(responseStringJson);
-            // Debug.Log("NexusSync response string:" + responseString);
+                string responseString = JsonConvert.DeserializeObject<string>(responseStringJson);
 
-            return responseString;
+                // Log the deserialized response string for debugging
+                Debug.Log("NexusSync response string: " + responseString);
+
+                return responseString;
+            }
+            catch (Exception e)
+            {
+                // Log any exceptions that occur during the request
+                Debug.LogError("Exception during NexusSyncTask: " + e.Message);
+                return null; // Return null or an empty string as a default value on error
+            }
         }
+
     }
 }
--- Assets/PUL/Managers/GameManager.cs
+++ Assets/PUL/Managers/GameManager.cs
@@ -9,12 +9,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.InputSystem.Utilities;
 
-namespace PUL
+namespace PUL2
 {
     public class GameManager : MonoBehaviour
     {
         //#region Singleton
 
+        public NexusClient nexusClient;
+        public ActiveOxideData aod;
+        public MenuManager menuManager;
         private static GameManager _instance;
 
         public static GameManager Instance
@@ -25,45 +28,10 @@ namespace PUL
                 {
                     Debug.LogError("GameManager is NULL");
                 }
-                
+
                 return _instance;
             }
         }
-
-
-        public List<GameObject> graphHolder = new List<GameObject>();
-
-        // TODO: We need to support more than a single graph of a single type, obviously, but we're here for now.
-        //public UhGraph codeGraph;
-        public RandomStartForceDirectedGraph codeGraph1;
-
-        public Graph codeGraph2;
-
-        public GameObject panelView;
-        
-        //required for DisplayTextOnPanelView, since this function is called in a loop. Really shady solution, but what can you do?
-        HashSet<string> processedLines = new HashSet<string>();
-
-        public GameObject transformationEdgePrefab;
-
-        
-        //string value refers to block ID. The List of GameObjects refers to all present in the path.
-        public IDictionary<string, List<GameObject>> totalPaths = new Dictionary<string, List<GameObject>>(); 
-
-        public CompVizStages cvs;
-
-        public List<GameObject> totalNodesAcrossGraphs = new List<GameObject>();
-
-        //refers to the list of current edges comprising a flow graph 
-        public List<GameObject> currentFlowGraphEdges = new List<GameObject>();
-
-        public InputAction rightHandRotateAnchor = null;
-
-        public NexusClient nexusClient;
-
-        public int graphTotal = 0;
-
-        bool isFinished = false;
 
         private void Awake()
         {
@@ -88,269 +56,21 @@ namespace PUL
         }
 
 
-        //works for the most part, but could potentially have some difficulty with edge case on B8? Everything else seems to be working, though.
-        public void GenerateAllPaths(IList<IList<IList<string>>> TotalRelationSet)
-        {
-            
-            
-            //Iterate through the set of all possible relations.
-            foreach(IList<IList<string>> curRelationSet in TotalRelationSet)
-            {
-                //for the values associated with stage 0 to be set as key values in totalPaths. Once incremented, this function will switch to
-                //"adding mode." where it adds all the current functions associated with the block in stage 0.
-                int stage = 0;
-                //this facillitates "adding mode." the current stage 0 block id is stored here, and every subsequent re
-                string curPathBlockID = null;
-                //Iterate through All Relations.
-                foreach (IList<string> blocksInCurRelationStage in curRelationSet)
-                {
-                      for(int i = 1; i < blocksInCurRelationStage.Count; i++)
-                        {
-                        if(stage  ==  0)
-                        {
-                            Debug.Log("CURRENT BLOCK TO BE ADDED " + blocksInCurRelationStage[i]);
-                            curPathBlockID = blocksInCurRelationStage[i];
-                            totalPaths[curPathBlockID] = new List<GameObject>();
-                        }
-                        else
-                        {
-                            GameObject curNode = null;
-                            //if the stage is greater than 0, assume that it's been added, and generate the path with the analogous node GameObject
-                            foreach (GameObject node in totalNodesAcrossGraphs)
-                            {
-                                if(node.GetComponent<Node>().nodeName == blocksInCurRelationStage[i])
-                                {
-                                     curNode = node;
-                                    break;
-                                }
-                            }
-                            if (curPathBlockID != null && curNode != null)
-                            {
-                                //totalPaths[curPathBlockID].Add(curNode);
-                            }
-                            else
-                            {
-                                Debug.LogError("WARNING: curPathBlock ID and or curNode is null! Check GenerateAllPaths()");
-                            }
-                        }
-                    }
-                      stage++;
-                    }
-                }    
-            
-        }   
-
-
-
-        //places the graphHolders in the world.
-        List<GameObject> instantiateGraphHolders(int graphTotal)
-        {
-            List<GameObject> graphList = new List<GameObject>();
-
-            Vector3 graphPos = Vector3.zero;
-            for (int i = 0; i < graphTotal; i++)
-            {
-                //Debug.Log("Running Graph Holder Instantiation!");
-                GameObject newGraph = new GameObject("graphHolder");
-                newGraph.transform.position = graphPos;
-                newGraph.transform.rotation = Quaternion.identity;
-                graphPos.x += 5;
-                graphList.Add(newGraph);
-            }
-            return graphList;
-        }
-        //instantiates the random stat force directed graph
-        void instantiateGraph1()
-        {
-            Vector3 graphPos = Vector3.zero;
-            foreach (GameObject graph in graphHolder)
-            {
-                codeGraph1 = graph.AddComponent<RandomStartForceDirectedGraph>() as RandomStartForceDirectedGraph;
-                codeGraph1.transform.localPosition = graphPos;
-                graphPos.x += 5;
-                codeGraph1.transform.localEulerAngles = Vector3.zero;
-            }
-            //use nexus to build this graph later on.
-        }
-
-        //instantiates the 2D Node graph.
-        void instantiateGraph2(ref List<GameObject> graphHolder)
-        {
-            Vector3 graphPos = Vector3.zero;
-            foreach (GameObject graph in graphHolder)
-            {
-                //Debug.Log("Adding Graph!");
-                codeGraph2 = graph.AddComponent<Graph>() as Graph;
-            }
-
-            if (cvs != null)
-            {
-                nexusClient.buildGraph2FromOxideBlocks(cvs, ref graphHolder);
-                fillTotalNodesAcrossGraphs();
-               // GenerateAllPaths(cvs.blockRelations);
-
-                //Debug.LogWarning("TotalNodesAcrossGraphs " + graphHolder.Count);
-
-            }
-
-           else
-            {
-                Debug.LogWarning("Error! CVS is Empty!");
-            }
-
-            
-        }
-
-        void fillTotalNodesAcrossGraphs()
-        {
-            foreach(GameObject graph in graphHolder)
-            {
-                foreach(Transform node in graph.transform)
-                {
-                    totalNodesAcrossGraphs.Add(node.gameObject);
-                }
-            }
-            
-        }
-
-        
 
         // Start is called before the first frame update
         void Start()
         {
             // Initialize Nexus client
             nexusClient = new NexusClient(this);
-
-
             nexusClient.NexusSessionInit();
-          //  graphTotal = cvs.stages.Count;
-            //Debug.Log("GameManager START");
-
-            // Initialize scene objects
-
-            // TODO: This probably needs to be a class member
-            
-
-            // codeGraph = graphHolder.AddComponent<UhGraph>() as UhGraph;
-
-            //instantiateGraph1();
-
-            //instantiateGraph2();
-            //findRightHandAnchor();
-
-            
         }
-        void findRightHandAnchor()
-        {
-            // Find rightHandRotateAnchor input action
-            // TODO: Find a direct route to the action instead of this tedious drill-down
-            // TODO: Learn the Unity Action-based input system better... I guess
-            InputActionManager iam = FindObjectOfType<InputActionManager>();
-            List<InputActionAsset> actionAssets = iam.actionAssets;
-            foreach (InputActionAsset iaa in actionAssets)
-            {
-                //Debug.Log("ASSET: " + iaa.name);
-                ReadOnlyArray<InputActionMap> actionMaps = iaa.actionMaps;
-                foreach (InputActionMap iamap in actionMaps)
-                {
-                    //Debug.Log("  MAP: " + iamap.name);
-                    ReadOnlyArray<InputAction> actions = iamap.actions;
-                    foreach (InputAction ia in actions)
-                    {
-                        //Debug.Log("    ACTION: " + ia.name);
-                        if (iamap.name == "XRI RightHand Interaction" && ia.name == "Rotate Anchor")
-                        {
-                            rightHandRotateAnchor = ia;
-                        }
-                    }
-                }
-            }
-        }
-        //displays text on the panelview slate. Works very similarly to DisplayTextOnNode in Node.cs. I need to outfit that script so it takes in buttons instead of text. might consolidate the two in the future.
-        public void DisplayTextOnPanelView(GameObject PanelView, GameObject sourceCodeGraph)
-        {
-            int i = 0;
-            int counter = 0;
-            GameObject buttonPrefab = PanelView.transform.GetChild(2).gameObject;
-         
-
-            foreach (Transform childNode in sourceCodeGraph.transform)
-            {
-                foreach (string line in childNode.gameObject.GetComponent<Node>().nodeValue)
-                {
-                    //Debug.LogWarning("PROCESSING LINE" + " " + line);
-                    if (processedLines.Contains(line))
-                    {
-                        continue; // Skip duplicate lines
-                    }
-
-                    if (i == 0)
-                    {
-                        PanelView.transform.GetChild(2).GetChild(3).gameObject.GetComponent<TextMeshPro>().text = line;
-                        PanelView.transform.GetChild(2).GetComponent<SourceCodeLineController>().blockID = childNode.gameObject.GetComponent<Node>().nodeName;
-                        i++;
-                    }
-                    else
-                    {
-                        GameObject newButtonObject = Instantiate(buttonPrefab, PanelView.transform);
-                        newButtonObject.transform.GetChild(3).gameObject.GetComponent<TextMeshPro>().text = line;
-                        newButtonObject.GetComponent <SourceCodeLineController>().blockID = childNode.gameObject.GetComponent<Node>().nodeName;
-
-                        // Adjust position with vertical spacing
-                        Vector3 newPosition = newButtonObject.transform.localPosition;
-                        newPosition.y -= 0.025f * i;
-                        newButtonObject.transform.localPosition = newPosition;
-                        i++;
-                    }
-       
-
-                    processedLines.Add(line); // Add the line to the processed set
-                }
-            }
-
-            
-        }
-
-
-        void rotateGraph()
-        {
-            foreach (GameObject graph in graphHolder)
-            {
-                // Rotate the code graph based on user input
-                Vector2 v = rightHandRotateAnchor.ReadValue<Vector2>();
-                //Debug.Log(v.x);
-                if (Math.Abs(v.x) > 0.7f)
-                {
-                    graph.transform.localEulerAngles = graph.transform.localEulerAngles + new Vector3(0, v.x * -0.5f, 0);
-                }
-                if (Math.Abs(v.y) > 0.7f)
-                {
-                    graph.transform.localPosition = graph.transform.localPosition + new Vector3(0, v.y * 10.0f, 0);
-                }
-
-            }
-        }
+        
 
         // Update is called once per frame
         void Update()
         {
-           
             // Sync with Nexus
             nexusClient.OnUpdate();
-
-            if (graphTotal != 0 && !isFinished)
-            {
-                isFinished = true;
-              
-                graphHolder = instantiateGraphHolders(graphTotal);
-                //Debug.Log(graphHolder[0]);
-                instantiateGraph2(ref graphHolder);
-                // Graph has to be a component within a GameObject for StartCoroutine to work
-            }
-            
-
-            // Update the graph
-            //codeGraph1.OnUpdate();
         }
 
         //#endregion
